cb55bd3a8228c7d8174ff48d99d857863248f196
xueyuan <xueyuan921@163.com>
2023-12-28 16:52:10 +0800

    2table 组件

--- jianghu-init/lib/json/init_page_1table.js
+++ jianghu-init/lib/json/init_page_1table.js
@@ -45,12 +45,13 @@ module.exports = class InitPage1Table extends CommandBase {
     this.info(`开始生成 ${table} 的 CRUD`);
     // 生成 vue
     const renderResult = await this.renderVue(jsonConfig);
+    await this.renderComonentVue(jsonConfig);
     if (renderResult) {
       this.success(`生成 ${table} 的 vue 文件完成`);
       // 数据库
       this.info(`开始生成 ${table} 的相关数据`);
       await this.modifyTable(table, pageId);
-      await this.modifyComponentResource(table, pageId, jsonConfig);
+      await this.modifyComponentResource(jsonConfig);
       this.success(`生成 ${table} 的相关数据完成`);
     } else {
       this.error(`生成 ${table} 的 vue 文件失败`);
@@ -91,16 +92,17 @@ module.exports = class InitPage1Table extends CommandBase {
     }
   }
 
-  async modifyComponentResource(table, pageId, jsonConfig) {
+  async modifyComponentResource(jsonConfig) {
     const knex = await this.getKnex();
     const templatePath = `${path.join(__dirname, '../../')}page-template-json/component`;
     const componentList = this.getUpdateDrawerComponentList(jsonConfig);
 
     // 循环 componentList 运行 sql
     for (const component of componentList) {
-      let resourceSql = fs.readFileSync(`${templatePath}/${component.replace('.html', '.sql')}`).toString();
-      resourceSql = resourceSql.replace(/\{\{pageId}}/g, pageId);
-      resourceSql = resourceSql.replace(/\{\{table}}/g, table);
+      let resourceSql = fs.readFileSync(`${templatePath}/${component.filename}.sql`).toString();
+      _.forEach(component.njkObj, (value, key) => {
+        resourceSql = resourceSql.replace(new RegExp(`{{${key}}}`, 'g'), value);
+      });
       
       // 插入数据
       for (const line of resourceSql.split('\n')) {
@@ -116,54 +118,8 @@ module.exports = class InitPage1Table extends CommandBase {
 
   }
 
-  functionToStr(obj) {
-    return JSON.stringify(obj, function(key, value) {
-      if (typeof value === 'function') {
-        return `__FUNC_START__${value.toString()}__FUNC_END__`;
-      }
-      return value;
-    }).replace(/"__FUNC_START__/g, '').replace(/__FUNC_END__"/g, '');
-  }
 
-  getUpdateDrawerComponentList(jsonConfig) {
-    const componentList = [];
-    const componentMap = {
-      'recordHistory': 'tableRecordHistory.html',
-    };
-    if (jsonConfig.updateDrawerContent && jsonConfig.updateDrawerContent.contentList) {
-      jsonConfig.updateDrawerContent.contentList.forEach(item => {
-        if (item.type != 'updateForm' && componentMap[item.type]) {
-          componentList.push(componentMap[item.type]);
-        }
-      });
-    }
-    return componentList;
-  }
-
-  /**
-   * 生成 vue
-   */
-  async renderVue(jsonConfig) {
-    const pageBakDir = `./app/view/pageBak`;
-    if (!fs.existsSync(pageBakDir)) fs.mkdirSync(pageBakDir);
-
-    const { table, pageId, pageType } = jsonConfig;
-    // 原样输出渲染data
-    if (jsonConfig.common && jsonConfig.common.data) {
-      _.forEach(jsonConfig.common.data, (value, key) => {
-        jsonConfig.common.data[key] = this.functionToStr(value);
-      });
-    }
-    const tableCamelCase = _.camelCase(table);
-    const filepath = `./app/view/page/${pageId}.html`;
-    const htmlBasePath = `${path.join(__dirname, '../../')}page-template-json/base.njk.html`;
-    const templatePath = `${path.join(__dirname, '../../')}page-template-json/1table-page`;
-    const componentPath = `${path.join(__dirname, '../../')}page-template-json/component`;
-    const templateTargetPath = `${templatePath}/${pageType}.njk.html`;
-    const listTemplate = fs.readFileSync(templateTargetPath)
-      .toString()
-      .replace(/\/\/===\/\/ /g, '')
-      .replace(/\/\/===\/\//g, '');
+  handleNunjucksEnv(templateTargetPath) {
     const nunjucksEnv = nunjucks.configure(templateTargetPath, {
       autoescape: false,
       tags: {
@@ -187,20 +143,154 @@ module.exports = class InitPage1Table extends CommandBase {
       const arrayStr = `[\n${arr.map(item => "  " + spaceStr + JSON.stringify(item).replace(/"([^"]+)":/g, '$1:') + ",\n").join("")}${spaceStr}]`;
       return `${key}: ${arrayStr}`;
     });
+    // 复杂变量包含函数或函数原样渲染
+    nunjucksEnv.addFilter('variableToVar', function(obj, k) {
+      let content = JSON.stringify(obj, function(key, value) {
+          if (typeof value === 'function') {
+              return `__FUNC_START__${value.toString()}__FUNC_END__`;
+          }
+          return value;
+      }, 2).replace(/"__FUNC_START__/g, '').replace(/__FUNC_END__"/g, '').replace(/\\n/g, '\n').replace(/\\/g, '').replace(/\n/g, '\n    ');
 
+      if (/^function\s*?\(/.test(content)) {
+        content = k + ': ' + content;
+      }
+      if (/^async\s+function\s*?\(/.test(content)) {
+        content = k + ': ' + content;
+      }
+      if (typeof obj == 'function') {
+        content = content.replace(/   /g, ' ');
+      }
+      return content;
+    });
+    nunjucksEnv.addFilter('camelToKebab', function(obj) {
+      return obj.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '\$1-\$2').toLowerCase();
+    });
+    return nunjucksEnv;
+  }
+
+  handleJsonConfig(jsonConfig) {
+    const { pageContent } = jsonConfig;
+    const defaultActionList = [
+      { text: "编辑", action: "startUpdateItem", icon: "mdi-pencil", color: "primary", },
+      { text: "删除", action: "deleteItem", icon: "mdi-delete", color: "error", },
+    ]
+    // pageContent.actionList push defaultActionList;
+    if (!pageContent.actionList) {
+      pageContent.actionList = defaultActionList;
+    }
+    
+  }
+
+  getUpdateDrawerComponentList(jsonConfig) {
+    const { table, pageId, primaryField } = jsonConfig;
+    const componentList = [];
+    const componentMap = {
+      'recordHistory': {filename: 'tableRecordHistory'},
+      '2table': {
+        filename: '2table',
+        require: ['table', 'primaryField'],
+      },
+    };
+    if (jsonConfig.updateDrawerContent && jsonConfig.updateDrawerContent.contentList) {
+      jsonConfig.updateDrawerContent.contentList.forEach(item => {
+        if (item.type != 'updateForm' && componentMap[item.type]) {
+          item.filename = componentMap[item.type].filename;
+          item.templateName = componentMap[item.type].filename;
+          if (componentMap[item.type].require) {
+            componentMap[item.type].require.forEach(key => {
+              if (!item[key]) {
+                this.error(`${item.type}组件 缺少参数 ${key}`);
+                throw new Error(`${item.type}组件 缺少参数 ${key}`);
+              }
+            });
+          }
+          item.njkObj = { 
+            table: table,
+            pageId
+          }
+          if (item.type == '2table' && item.table) {
+            const tableCamelCaseB = _.camelCase(item.table);
+            const tableCamelCaseA = _.camelCase(table);
+            const templateName = `${tableCamelCaseB}Of${tableCamelCaseA.replace(tableCamelCaseA[0],tableCamelCaseA[0].toUpperCase())}`;
+            item.njkObj = { 
+              tableCamelCaseA: _.camelCase(table), 
+              tableCamelCaseB, 
+              tableA: table, 
+              tableB: item.table,
+              nameA: table, 
+              nameB: item.table,
+              primaryFieldA: primaryField,
+              primaryFieldB: item.primaryField,
+              pageId,
+              templateName
+            }
+            item.templateName = templateName;
+          }
+          componentList.push(item);
+        }
+      });
+    }
+    return componentList;
+  }
+
+  async renderComonentVue(jsonConfig) {
+    const componentList = this.getUpdateDrawerComponentList(jsonConfig);
+    if (!componentList.length) return;
+
+    const componentPath = `${path.join(__dirname, '../../')}page-template-json/component`;
+    if (componentList.length) {
+      if (!fs.existsSync(`./app/view/component`)) fs.mkdirSync(`./app/view/component`);
+    }
+
+    for( const item of componentList) {
+      let componentHtml = fs.readFileSync(componentPath + '/' + item.filename + '.html')
+        .toString()
+        .replace(/\/\/===\/\/ /g, '')
+        .replace(/\/\/===\/\//g, '');
+      if (item.type == '2table' && item.table) {
+        const tableBFields = await this.getTableFields(item.table);
+        componentHtml = nunjucks.renderString(componentHtml, {...item.njkObj, tableBFields});
+      }
+
+      fs.writeFileSync(`./app/view/component/${item.templateName}.html`, componentHtml);
+    }
+  }
+  /**
+   * 生成 vue
+   */
+  async renderVue(jsonConfig) {
+    const pageBakDir = `./app/view/pageBak`;
+    if (!fs.existsSync(pageBakDir)) fs.mkdirSync(pageBakDir);
+
+    const { table, pageId, pageType } = jsonConfig;
+    const tableCamelCase = _.camelCase(table);
+    const filepath = `./app/view/page/${pageId}.html`;
+    const htmlBasePath = `${path.join(__dirname, '../../')}page-template-json/base.njk.html`;
+    const templatePath = `${path.join(__dirname, '../../')}page-template-json/1table-page`;
+    const templateTargetPath = `${templatePath}/${pageType}.njk.html`;
+    const listTemplate = fs.readFileSync(templateTargetPath)
+      .toString()
+      .replace(/\/\/===\/\/ /g, '')
+      .replace(/\/\/===\/\//g, '');
+
+    // 初始化 njk 模板标签、filter
+    this.handleNunjucksEnv(templateTargetPath);
+    this.handleJsonConfig(jsonConfig);
+    
     const componentList = this.getUpdateDrawerComponentList(jsonConfig);
     const htmlBase = fs.readFileSync(htmlBasePath);
     let htmlUser = fs.existsSync(filepath) ? fs.readFileSync(filepath) : htmlBase;
     const htmlGenerate = nunjucks.renderString(listTemplate, { tableCamelCase, ...jsonConfig, componentList });
     const bakFilePath = await this.handleViewBak(pageId, filepath);
 
-    // 组件添加文件夹、文件
-    if (componentList.length) {
-      if (!fs.existsSync(`./app/view/component`)) fs.mkdirSync(`./app/view/component`);
+    
+    // 生成 md
+    if (jsonConfig.headContent && jsonConfig.headContent.helpDrawer) {
+      const mdPath = `./app/view/pageDoc`;
+      if (!fs.existsSync(mdPath)) fs.mkdirSync(mdPath);
+      fs.writeFileSync(`${mdPath}/${pageId}.md`, `# ${pageId}页面`);
     }
-    componentList.forEach(item => {
-      fs.writeFileSync(`./app/view/component/${item}`, fs.readFileSync(componentPath + '/' + item));
-    });
 
     // fs.writeFileSync(filepath, htmlUser);
     fs.writeFileSync(filepath, htmlGenerate); // 测试  
@@ -250,6 +340,40 @@ module.exports = class InitPage1Table extends CommandBase {
             resolve(stdout);
         });
     });
+  }
+
+  /**
+   * 获取数据库表所有原生字段
+   * @param {String} table
+   * @returns
+   */
+  async getTableFields(table) {
+    const knex = await this.getKnex();
+    const result = await knex.select('COLUMN_NAME', 'COLUMN_COMMENT').from('INFORMATION_SCHEMA.COLUMNS').where({
+      TABLE_SCHEMA: this.dbSetting.database,
+      TABLE_NAME: table,
+    });
+
+    const defaultColumn = [ 'operation', 'operationByUserId', 'operationByUser', 'operationAt' ];
+    for (const column of defaultColumn) {
+      await knex.schema.hasColumn(table, column).then(exists => {
+        if (!exists) {
+          return knex.schema.table(table, t => {
+            this.info(`创建依赖字段：${column}`);
+            t.string(column);
+          });
+        }
+      });
+    }
+
+    const columns = result.map(column => {
+      return {
+        COLUMN_NAME: column.COLUMN_NAME,
+        COLUMN_COMMENT: (column.COLUMN_COMMENT || column.COLUMN_NAME || '').split(';')[0].split('；')[0].split(':')[0],
+      };
+    });
+
+    return columns;
   }
 
 };
