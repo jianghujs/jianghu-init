296cb56a79bd3a56fb0087ed4f85b0446a109ffb
colin <colin>
2024-04-12 21:41:41 +0800

    去掉 author 和 desc输入

--- project-dist/jianghu-boilerplate-enterprise/index.js
+++ project-dist/jianghu-boilerplate-enterprise/index.js
@@ -5,12 +5,6 @@ module.exports = {
   name: {
     desc: 'project name',
   },
-  description: {
-    desc: 'project description',
-  },
-  author: {
-    desc: 'project author',
-  },
   dbPrefix: {
     desc: 'prefix of database',
   },
