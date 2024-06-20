12f9afe24c80c0909fefbc0ae59fcec2a73584b3
xueyuan <xueyuan921@163.com>
2024-06-20 17:23:41 +0800

    app/view/common 目录判断创建

--- jianghu-init/lib/init_by_json.js
+++ jianghu-init/lib/init_by_json.js
@@ -241,9 +241,9 @@ module.exports = class InitByJsonCommand extends CommandBase {
   // 复制jianghuJs公共组件和样式
   async copyJianghuJs() {
     // 如果没有jianghuJs 目录，则创建
+    if (!fs.existsSync('./app/view/common')) fs.mkdirSync('./app/view/common');
     if (!fs.existsSync('./app/view/common/jianghuJs')) fs.mkdirSync('./app/view/common/jianghuJs');
     if (!fs.existsSync('./app/view/component/jianghuJs')) fs.mkdirSync('./app/view/component/jianghuJs');
-
 
     // 复制通用样式和组件
     this.copyDir(`${path.join(__dirname, '../')}page-template-json/component/jianghuJs`, './app/view/component/jianghuJs');
